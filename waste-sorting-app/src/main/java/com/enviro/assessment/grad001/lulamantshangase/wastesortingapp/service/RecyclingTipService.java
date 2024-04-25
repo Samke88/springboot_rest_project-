be76c9f0d6e9dbecee4f4f195fe91810cb9e0086
Lulama Ntshangase <111311152+Samke88@users.noreply.github.com>
2024-04-25 02:11:34 +0200

    Update RecyclingTipService.java

--- waste-sorting-app/src/main/java/com/enviro/assessment/grad001/lulamantshangase/wastesortingapp/service/RecyclingTipService.java
+++ waste-sorting-app/src/main/java/com/enviro/assessment/grad001/lulamantshangase/wastesortingapp/service/RecyclingTipService.java
@@ -1,27 +1,4 @@
-// package com.enviro.assessment.grad001.lulamantshangase.wastesortingapp.service;
-// import com.enviro.assessment.grad001.lulamantshangase.wastesortingapp.model.RecyclingTip;
-// import org.springframework.stereotype.Service;
-// import java.util.HashMap;
-// import java.util.Map;
-
-// @Service
-// public class RecyclingTipService {
-
-//     private static final Map<String, RecyclingTip> recyclingTips = new HashMap<>();
-
-//     static {
-//         recyclingTips.put("Plastic", new RecyclingTip("Recycle plastic bottles", "Recycle plastic containers"));
-//         recyclingTips.put("Paper", new RecyclingTip("Recycle newspapers", "Recycle cardboard"));
-//         // Add more recycling tips as needed
-//     }
-
-//     public RecyclingTip getRecyclingTipByWasteCategory(String wasteCategory) {
-//         return recyclingTips.getOrDefault(wasteCategory, new RecyclingTip("No recycling tip available", ""));
-//     }
-// }
-
 package com.enviro.assessment.grad001.lulamantshangase.wastesortingapp.service;
-
 import com.enviro.assessment.grad001.lulamantshangase.wastesortingapp.model.RecyclingTip;
 import org.springframework.stereotype.Service;
 
